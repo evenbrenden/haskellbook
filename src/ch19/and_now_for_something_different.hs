13c9e16b3ecb8e02447eb0a04b8e601f8050a4b6
Even Brenden <evenbrenden@gmail.com>
2019-01-18 15:03:18 +0100

    And now for something different

---
+++ src/ch19/and_now_for_something_different.hs
@@ -0,0 +1,10 @@
+module Web.Shipping.Utils ((<||>)) where
+
+import Control.Applicative (liftA2)
+
+(<||>) :: (a -> Bool)
+       -> (a -> Bool)
+       -> a
+       -> Bool
+(<||>) = liftA2 (||)
+-- (<||>) f g = (||) <$> f <*> g
