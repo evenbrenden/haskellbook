3fd106a9406e2ec4a0ea8b328d05140547cc4504
Even Brenden <evenbrenden@gmail.com>
2019-04-19 11:40:24 +0200

    Chapter 23: Chapter Exercises

---
+++ src/ch23/ChapterExercises.hs
@@ -0,0 +1,18 @@
+module ChapterExercises where
+
+import WriteStateForYourself
+
+get :: Moi s s
+get = Moi $ \s -> (s, s)
+
+put :: s -> Moi s ()
+put s1 = Moi $ \s2 -> ((), s1)
+
+exec :: Moi s a -> s -> s
+exec (Moi sa) s = snd (sa s)
+
+eval :: Moi s a -> s -> a
+eval (Moi sa) s = fst (sa s)
+
+modify :: (s -> s) -> Moi s ()
+modify ss = Moi $ \s -> ((), ss s)
