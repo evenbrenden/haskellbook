e9f7815a2bf431a87b95b4d0e752ac9f185bacb9
Even Brenden <evenbrenden@gmail.com>
2019-06-14 08:33:38 +0200

    Chapter 27: Will printing this expression result in bottom?

---
+++ src/ch27/WillPrintingThisExpressionResultInBottom.hs
@@ -0,0 +1,28 @@
+-- 1
+snd (undefined, 1)
+-- No.
+
+-- 2
+let x = undefined
+let y = x `seq` 1 in snd (x, y)
+-- Yes.
+
+-- 3
+length $ [1..5] ++ undefined
+-- Yes.
+
+-- 4
+length $ [1..5] ++ [undefined]
+-- No.
+
+-- 5
+const 1 undefined
+-- No.
+
+-- 6
+const 1 (undefined `seq` 1)
+-- No.
+
+-- 7
+const undefined 1
+-- Yes.
