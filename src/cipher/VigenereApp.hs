c3f4951401678dfba3ded751197f76daae304ba1
Even Brenden <evenbrenden@gmail.com>
2019-06-16 08:46:48 +0200

    Merge Vigenere and Unvigenere apps

--- src/cipher/VigenereApp.hs
+++ src/cipher/VigenereApp.hs
@@ -4,8 +4,8 @@ import qualified Cipher as C
 import System.Exit (exitSuccess)
 import System.IO
 
-main :: IO ()
-main = do
+vigenere :: IO ()
+vigenere = do
   hSetBuffering stdout NoBuffering
   putStrLn $ "Vigenere!"
   putStr $ "Word to encode: "
@@ -13,4 +13,15 @@ main = do
   putStr $ "Key for encoding your word: "
   key <- getLine
   putStrLn $ "Encoded word: " ++ (C.vigenere word key)
+  exitSuccess
+
+unVigenere :: IO ()
+unVigenere = do
+  hSetBuffering stdout NoBuffering
+  putStrLn $ "Un-Vigenere!"
+  putStr $ "Word to decode: "
+  word <- getLine
+  putStr $ "Key for decoding your word: "
+  key <- getLine
+  putStrLn $ "Decoded word: " ++ (C.unVigenere word key)
   exitSuccess
