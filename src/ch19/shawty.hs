101dd58a0327106dc2650e57c97f33e1626f6616
Even Brenden <evenbrenden@gmail.com>
2022-05-16 15:25:42 +0200

    Format shawty.hs

--- src/ch19/shawty.hs
+++ src/ch19/shawty.hs
@@ -2,18 +2,22 @@
 
 module Shawty where
 
-import Control.Monad (replicateM)
-import Control.Monad.IO.Class (liftIO)
-import qualified Data.ByteString.Char8 as BC
-import Data.Text.Encoding (decodeUtf8, encodeUtf8)
-import qualified Data.Text.Lazy as TL
-import qualified Database.Redis as R
-import Network.URI (URI, parseURI)
-import qualified System.Random as SR
-import Web.Scotty
+import           Control.Monad                  ( replicateM )
+import           Control.Monad.IO.Class         ( liftIO )
+import qualified Data.ByteString.Char8         as BC
+import           Data.Text.Encoding             ( decodeUtf8
+                                                , encodeUtf8
+                                                )
+import qualified Data.Text.Lazy                as TL
+import qualified Database.Redis                as R
+import           Network.URI                    ( URI
+                                                , parseURI
+                                                )
+import qualified System.Random                 as SR
+import           Web.Scotty
 
 alphaNum :: String
-alphaNum = ['A'..'Z'] ++ ['0'..'9']
+alphaNum = ['A' .. 'Z'] ++ ['0' .. '9']
 
 randomElement :: String -> IO Char
 randomElement xs = do
@@ -23,57 +27,35 @@ randomElement xs = do
   return (xs !! randomDigit)
 
 shortyGen :: IO [Char]
-shortyGen =
-  replicateM 7 (randomElement alphaNum)
+shortyGen = replicateM 7 (randomElement alphaNum)
 
-saveURI :: R.Connection
-        -> BC.ByteString
-        -> BC.ByteString
-        -> IO (Either R.Reply R.Status)
-saveURI conn shortURI uri =
-  R.runRedis conn $ R.set shortURI uri
+saveURI
+  :: R.Connection
+  -> BC.ByteString
+  -> BC.ByteString
+  -> IO (Either R.Reply R.Status)
+saveURI conn shortURI uri = R.runRedis conn $ R.set shortURI uri
 
-getURI :: R.Connection
-       -> BC.ByteString
-       -> IO (Either R.Reply (Maybe BC.ByteString))
-getURI conn shortURI =
-  R.runRedis conn $ R.get shortURI
+getURI
+  :: R.Connection -> BC.ByteString -> IO (Either R.Reply (Maybe BC.ByteString))
+getURI conn shortURI = R.runRedis conn $ R.get shortURI
 
 linkShorty :: String -> String
 linkShorty shorty =
-  concat
-  [ "<a href=\""
-  , shorty
-  , "\">Copy and paste your short URL</a>"
-  ]
+  concat ["<a href=\"", shorty, "\">Copy and paste your short URL</a>"]
 
-shortyCreated :: Show a
-              => a
-              -> String
-              -> TL.Text
+shortyCreated :: Show a => a -> String -> TL.Text
 shortyCreated resp shawty =
-  TL.concat [ TL.pack (show resp)
-            , " shorty is: "
-            , TL.pack (linkShorty shawty)
-            ]
+  TL.concat [TL.pack (show resp), " shorty is: ", TL.pack (linkShorty shawty)]
 
 shortyAintUri :: TL.Text -> TL.Text
 shortyAintUri uri =
-  TL.concat
-    [ uri
-    , " isn't a URL,"
-    , " did you forget http://?"
-    ]
+  TL.concat [uri, " isn't a URL,", " did you forget http://?"]
 
 shortyFound :: TL.Text -> TL.Text
-shortyFound tbs =
-  TL.concat
-    [ "<a href=\""
-    , tbs, "\">"
-    , tbs, "</a>" ]
+shortyFound tbs = TL.concat ["<a href=\"", tbs, "\">", tbs, "</a>"]
 
-app :: R.Connection
-    -> ScottyM ()
+app :: R.Connection -> ScottyM ()
 app rConn = do
   get "/" $ do
     uri <- param "uri"
@@ -83,28 +65,29 @@ app rConn = do
       Just _ -> do
         shawty <- liftIO shortyGen
         let shorty = BC.pack shawty
-            uri' = encodeUtf8 (TL.toStrict uri)
+            uri'   = encodeUtf8 (TL.toStrict uri)
         uri <- liftIO (getURI rConn shorty)
         case uri of
-          Left reply ->
-            text (TL.pack (show reply))
-          Right mbBS -> case mbBS of
+          Left  reply -> text (TL.pack (show reply))
+          Right mbBS  -> case mbBS of
             Nothing -> do
               resp <- liftIO (saveURI rConn shorty uri')
               html (shortyCreated resp shawty)
-            Just _ -> raise "We generated a shawty that already exists! Just refresh to try again."
+            Just _ ->
+              raise
+                "We generated a shawty that already exists! Just refresh to try again."
       Nothing -> text (shortyAintUri uri)
   get "/:short" $ do
     short <- param "short"
-    uri <- liftIO (getURI rConn short)
+    uri   <- liftIO (getURI rConn short)
     case uri of
-      Left reply ->
-        text (TL.pack (show reply))
-      Right mbBS -> case mbBS of
+      Left  reply -> text (TL.pack (show reply))
+      Right mbBS  -> case mbBS of
         Nothing -> text "URI not found"
         Just bs -> html (shortyFound tbs)
-          where tbs :: TL.Text
-                tbs = TL.fromStrict (decodeUtf8 bs)
+         where
+          tbs :: TL.Text
+          tbs = TL.fromStrict (decodeUtf8 bs)
 
 main :: IO ()
 main = do
